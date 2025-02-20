074df790d92f1a2c09b102b67f602b06a5296bbc
MejiaBravoLuis <lmebravo6@gmail.com>
2025-02-20 16:04:16 -0600

    Se añadió el modelo publicaciones

---
+++ src/publications/publicavtion.model.js
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+
+const PublicationSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Title is required"],
+            maxLength: [50, "Can't exceed 50 characters"],
+            minLength: [5, "5 characters min"],
+            trim: true
+        },
+        ppalText: {
+            type: String,
+            required: [true, "Add a text to your publication"],
+            maxLength: [1000, "Can't exceed 1000 characters"],
+            minLength: [10, "10 characters min"],
+            trim: true
+        },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, "User is required"]
+        },
+        category: {
+            type: Schema.type.ObjectId,
+            ref: "Category",
+            required: [true, "Add a category"]
+        },
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: Comment,
+            },
+        ],
+        status: {
+            Boolean: true,
+            default: true
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    },
+);
+
+export default model ("Publication", PublicationSchema)
